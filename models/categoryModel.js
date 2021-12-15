586fb97cc1e4a49cdb8f01bc78f3f8331bbf07ca
unknown <panu.raikkonen@metropolia.fi>
2021-12-15 14:37:01 +0200

    update

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -7,7 +7,7 @@ const promisePool = pool.promise();
 const getAllCategory = async (next) => {
   try {
     const [rows] = await promisePool.execute(
-      'SELECT category.id, name, description FROM category JOIN post ON category.id = post.category GROUP BY name'
+      'SELECT category.id, name, description FROM category JOIN post ON category.id = post.category GROUP BY name DESC'
     );
     return rows;
   } catch (e) {
@@ -19,9 +19,7 @@ const getAllCategory = async (next) => {
 const getCategory = async (id, next) => {
   try {
     const [rows] = await promisePool.execute(
-      'SELECT id, name, description FROM category JOIN post ON category.id = post.category WHERE id = ? GROUP BY name',
-      [id]
-    );
+      'SELECT name, description FROM category WHERE id = ?', [id]);
     return rows;
   } catch (e) {
     console.error('getCategory error', e.message);
--- models/postModel.js
+++ models/postModel.js
@@ -1,31 +1,33 @@
-'use strict';
-const { user_get } = require('../controllers/userController');
-const pool = require('../database/db');
-const { httpError } = require('../utils/errors');
+"use strict";
+const { user_get } = require("../controllers/userController");
+const pool = require("../database/db");
+const { httpError } = require("../utils/errors");
 const promisePool = pool.promise();
 
-const getAllPosts = async (next) => {
+const getAllPosts = async (id, next) => {
   try {
-    const [rows] = await promisePool.execute(`
-	SELECT 
-	post.id,
-	title, 
-	content, 
-	post.img,
-  post.created,
-	post.category,
-	post.owner, 
-	post.parent, 
-	user.username as ownername
-	FROM post 
-	JOIN user ON
-	post.owner = user.id
-  ORDER BY created DESC
- `);
+    const [rows] = await promisePool.execute(
+      `SELECT
+       post.id, 
+       title,
+       content,
+       post.img,
+       post.created,
+       post.category,
+       post.owner,
+       user.username AS ownername
+       FROM post 
+       JOIN user
+       ON post.owner = user.id
+       INNER JOIN category 
+       ON post.category = category.id 
+       WHERE post.parent IS NULL 
+       AND category.id = ?
+       ORDER BY created DESC`, [id]);
     return rows;
   } catch (e) {
-    console.error('getAllPosts error', e.message);
-    next(httpError('Database error', 500));
+    console.error("getAllPosts error", e.message);
+    next(httpError("Database error", 500));
   }
 };
 
@@ -33,75 +35,74 @@ const getPost = async (id, next) => {
   try {
     const [rows] = await promisePool.execute(
       `
-	SELECT 
-	post.id,
-	title, 
-	content, 
-  post.img,
-  post.created,
-	post.category,
-	post.owner, 
-	post.parent, 
-  user.username as ownername
-	FROM post 
-	JOIN user ON 
-	post.owner = user.id
-	WHERE post.id = ?
-  ORDER BY created DESC`,
+      SELECT 
+      post.id,
+      title, 
+      content, 
+      post.img,
+      post.created,
+      post.category,
+      owner, 
+      parent, 
+      user.username as ownername
+      FROM post 
+      JOIN user ON
+      post.owner = user.id
+	    WHERE post.id = ?`,
       [id]
     );
     return rows;
   } catch (e) {
-    console.error('getPost error', e.message);
-    next(httpError('Database error', 500));
+    console.error("getPost error", e.message);
+    next(httpError("Database error", 500));
   }
 };
 
 const addPost = async (title, content, img, owner, category, next) => {
   try {
     const [rows] = await promisePool.execute(
-      'INSERT INTO post (title, content, img, owner, category) VALUES (?, ?, ?, ?, ?)',
+      "INSERT INTO post (title, content, img, owner, category) VALUES (?, ?, ?, ?, ?)",
       [title, content, img, owner, category]
     );
     return rows;
   } catch (e) {
-    console.error('addPost error', e.message);
-    next(httpError('Database error', 500));
+    console.error("addPost error", e.message);
+    next(httpError("Database error", 500));
   }
 };
 
 const modifyPost = async (title, content, img, id, role, next) => {
   let sql =
-    'UPDATE post SET title = ?, content = ?, img = ? WHERE id = ? AND owner = ?;';
+    "UPDATE post SET title = ?, content = ?, img = ? WHERE id = ? AND owner = ?;";
   let params = [title, content, img, id, owner];
   if (role === 0) {
     sql =
-      'UPDATE post SET title = ?, content = ?, img = ? WHERE id = ? AND owner = ?;';
+      "UPDATE post SET title = ?, content = ?, img = ? WHERE id = ? AND owner = ?;";
     params = [title, content, img, id, owner];
   }
-  console.log('sql', sql);
+  console.log("sql", sql);
   try {
     const [rows] = await promisePool.execute(sql, params);
     return rows;
   } catch (e) {
-    console.error('addPost error', e.message);
-    next(httpError('Database error', 500));
+    console.error("addPost error", e.message);
+    next(httpError("Database error", 500));
   }
 };
 
 const deletePost = async (id, owner_id, role, next) => {
-  let sql = 'DELETE FROM post WHERE id = ? AND owner = ?';
+  let sql = "DELETE FROM post WHERE id = ? AND owner = ?";
   let params = [id, owner_id];
   if (role === 1) {
-    sql = 'DELETE FROM post WHERE id = ?';
+    sql = "DELETE FROM post WHERE id = ?";
     params = [id];
   }
   try {
     const [rows] = await promisePool.execute(sql, params);
     return rows;
   } catch (e) {
-    console.error('deletePost error', e.message);
-    next(httpError('Database error', 500));
+    console.error("deletePost error", e.message);
+    next(httpError("Database error", 500));
   }
 };
 
